6a47880a8451efc91c24ce0a8ffbe7c2c450fbda
Karen Kamal <karenkamal24@gmail.com>
2025-09-25 01:12:55 +0300

    edit InstructorService

--- app/Services/InstructorService.php
+++ app/Services/InstructorService.php
@@ -3,25 +3,17 @@
 namespace App\Services;
 
 use App\Models\Instructor;
-use Stevebauman\Location\Facades\Location;
 
 class InstructorService
 {
     public function getAllInstructors(string $ip, string $lang = 'en')
     {
-       
-        $position = Location::get($ip) ?? (object) ['countryCode' => 'US'];
-        $countryCode = $position->countryCode ?? 'US';
-
-        $instructors = Instructor::all()->map(function ($instructor) use ($lang, $countryCode) {
+        $instructors = Instructor::all()->map(function ($instructor) use ($lang) {
 
 
             $images = collect($instructor->images ?? [])->map(function($img) {
-                if (isset($img['image'])) {
-                    $img['image'] = asset('storage/' . $img['image']);
-                }
-                return $img;
-            });
+                return isset($img['image']) ? asset('storage/' . $img['image']) : null;
+            })->filter();
 
             return [
                 'id' => $instructor->id,
@@ -29,15 +21,12 @@ class InstructorService
                 'specialty' => $lang === 'ar' ? $instructor->specialty_ar : $instructor->specialty_en,
                 'description' => $lang === 'ar' ? $instructor->description_ar : $instructor->description_en,
                 'experience' => $instructor->experience,
-                'images' => $images,
+                'images' => $images, // دلوقتي مجرد array من روابط الصور
                 'contacts' => $instructor->contacts ?? [],
                 'students_count' => 0,
             ];
         });
 
-        return [
-            'status' => 'success',
-            'data' => $instructors
-        ];
+        return $instructors;
     }
 }
